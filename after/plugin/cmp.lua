bfd89ab6821e8d7594e7c18be31e3841130cb981
Lalaluke413 <lukevlloyd@gmail.com>
2023-09-06 00:43:19 -0500

    good cmp bindings

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -4,11 +4,6 @@ local luasnip = require('luasnip')
 -- load friendly-snippets
 require("luasnip/loaders/from_vscode").lazy_load()
 
-local check_backspace = function()
-  local col = vim.fn.col(".") - 1
-  return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-end
-
 cmp.setup({
 
   snippet = {
@@ -18,55 +13,35 @@ cmp.setup({
   },
 
   mapping = { -- default
-    ['<C-n>'] = {
-      i = function()
-        if cmp.visible() then
-          cmp.select_next_item()
-        else
-          cmp.complete()
-        end
-      end,
-    },
-    ['<C-p>'] = {
-      i = function()
-        if cmp.visible() then
-          cmp.select_prev_item()
-        else
-          cmp.complete()
-        end
-      end,
-    },
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-e>'] = cmp.mapping.abort(),
+
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expandable() then
-        luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif check_backspace() then
-        fallback()
       else
         fallback()
       end
-    end, {
-      "i",
-      "s",
-      "c",
-    }),
+    end, {"i", "s",}),
+
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
       else
         fallback()
       end
-    end, {
-      "i",
-      "s",
-      "c",
+    end, {"i", "s",}),
+
+    ["<CR>"] = cmp.mapping({
+      i = function(fallback)
+        if cmp.visible() and cmp.get_active_entry() then
+          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+        elseif luasnip.jumpable() then
+          luasnip.jump(1)
+        else
+          fallback()
+        end
+      end,
+      s = cmp.mapping.confirm({ select = true }),
+      c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
     }),
   },
 
